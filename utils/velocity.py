d9cace995d65e5d7f6c6383cee41952891e976c5
kuodrek <kuodrek@gmail.com>
2022-08-07 20:41:31 -0300

    added bound vortex verification; added ground effect calculation function

--- utils/velocity.py
+++ utils/velocity.py
@@ -3,9 +3,7 @@ import numpy.linalg as npla
 
 
 # Function to calculate the induced velocity caused by a vortex panel in a point
-def get_induced_velocity(collocation_point, vertice_point_1, vertice_point_2, v_inf, mac, h, same_panel_check): 
-    ri1j = np.zeros(3)
-    ri2j = np.zeros(3)
+def get_induced_velocity(collocation_point, vertice_point_1, vertice_point_2, v_inf, mac): 
     velocity_ij = np.zeros(3)
 
     ri1j = collocation_point - vertice_point_1
@@ -17,15 +15,51 @@ def get_induced_velocity(collocation_point, vertice_point_1, vertice_point_2, v_
     r2_cross_prod = np.cross(v_inf, ri2j)
     r1_dot_prod = np.dot(v_inf, ri1j)
     r2_dot_prod = np.dot(v_inf, ri2j)
-    
+    r12_cross_prod = np.cross(ri1j, ri2j)
+    r12_dot_prod = np.dot(ri1j, ri2j)
+
+    bound_vortex_den = (ri1j_abs*ri2j_abs*(ri1j_abs*ri2j_abs+r12_dot_prod))
+
     velocity_ij = mac / (4 * np.pi) * \
         ( r2_cross_prod / (ri2j_abs*(ri2j_abs-r2_dot_prod)) \
         - r1_cross_prod / (ri1j_abs*(ri1j_abs-r1_dot_prod)) )
     
-    if not same_panel_check:
-        r12_cross_prod = np.cross(ri1j, ri2j)
-        r12_dot_prod = np.dot(ri1j, ri2j)
+    if bound_vortex_den != 0:
         velocity_ij +=  mac / (4 * np.pi) * \
-            ( (ri1j_abs+ri2j_abs) * r12_cross_prod / (ri1j_abs*ri2j_abs*(ri1j_abs*ri2j_abs+r12_dot_prod)) )
+            ( (ri1j_abs+ri2j_abs) * r12_cross_prod / bound_vortex_den )
+    
+    return velocity_ij
+
+
+# Induced velocity calculation considering ground effect through reflection method
+def get_induced_velocity_ground_effect(collocation_point, vertice_point_1, vertice_point_2, v_inf, mac, h):
+    velocity_ij = np.zeros(3)
+
+    v_inf_ge = v_inf
+    v_inf_ge[2] = -v_inf_ge[2]
+
+    ri1j = collocation_point - vertice_point_1
+    ri1j[2] = collocation_point[2] - (2*h + 2*vertice_point_1[2])
+    ri2j = collocation_point - vertice_point_2
+    ri2j[2] = collocation_point[2] - (2*h + 2*vertice_point_2[2])
+
+    ri1j_abs = npla.norm(ri1j)
+    ri2j_abs = npla.norm(ri2j)
+    r1_cross_prod = np.cross(v_inf_ge, ri1j)
+    r2_cross_prod = np.cross(v_inf_ge, ri2j)
+    r1_dot_prod = np.dot(v_inf_ge, ri1j)
+    r2_dot_prod = np.dot(v_inf_ge, ri2j)
+    r12_cross_prod = np.cross(ri1j, ri2j)
+    r12_dot_prod = np.dot(ri1j, ri2j)
+
+    bound_vortex_den = (ri1j_abs*ri2j_abs*(ri1j_abs*ri2j_abs+r12_dot_prod))
+
+    velocity_ij = mac / (4 * np.pi) * \
+        ( r2_cross_prod / (ri2j_abs*(ri2j_abs-r2_dot_prod)) \
+        - r1_cross_prod / (ri1j_abs*(ri1j_abs-r1_dot_prod)) )
+    
+    if bound_vortex_den != 0:
+        velocity_ij +=  mac / (4 * np.pi) * \
+            ( (ri1j_abs+ri2j_abs) * r12_cross_prod / bound_vortex_den )
     
     return velocity_ij
