3891855b073591fe09ca7c11da0d3287c0cfb8c5
kuodrek <kuodrek@gmail.com>
2022-08-07 20:41:51 -0300

    updated inputs for testing

--- teste_vind.py
+++ teste_vind.py
@@ -17,10 +17,8 @@ asa = Wing(
 asa.generate_mesh()
 # collocation_point, vertice_point_1, vertice_point_2, v_inf, mac, h, same_panel_check)
 cp = asa.collocation_points[0]
-vp_1 = asa.vertice_points[1]
-vp_2 = asa.vertice_points[2]
+vp_1 = asa.vertice_points[0]
+vp_2 = asa.vertice_points[1]
 MAC = asa.MAC
 v_inf = np.array([0.9947, 0, -0.1028])
-h = 0
-same_panel_check = False
-get_induced_velocity(cp, vp_1, vp_2, v_inf, MAC, h, same_panel_check)
+get_induced_velocity(cp, vp_1, vp_2, v_inf, MAC)
