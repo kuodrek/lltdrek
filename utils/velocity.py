bd2cab066c975133c09f88b85326fb1b9bb25d4d
kuodrek <kuodrek@gmail.com>
2022-08-07 18:30:52 -0300

    updated velocity induced calculation function

--- utils/velocity.py
+++ utils/velocity.py
@@ -1,20 +1,32 @@
 import numpy as np
+import numpy.linalg as npla
 
-def get_div(collocation_point, vertice_point_1, vertice_point_2, v_inf, mac, h, same_panel_check): # Function to calculate induced velocities between 
+
+# Function to calculate the induced velocity caused by a vortex panel in a point in space
+def get_induced_velocity(collocation_point, vertice_point_1, vertice_point_2, v_inf, mac, h, same_panel_check): 
     ri1j = np.zeros(3)
     ri2j = np.zeros(3)
     velocity_ij = np.zeros(3)
+    
+    # numpy way
+    ri1j = collocation_point - vertice_point_1
+    ri2j = collocation_point - vertice_point_2
 
-    for k in range(3):
-        ri2j[k] = collocation_point[k] - vertice_point_2[k]
-        ri1j[k] = collocation_point[k] - vertice_point_1[k]
-
-    r1_cross_product = np.cross(v_inf, ri1j)
-    r2_cross_product = np.cross(v_inf, ri2j)
-    r1_dot_product = np.dot(v_inf, ri1j)
-    r2_dot_product = np.dot(v_inf, ri2j)
-
-    for k in range(3):
-        velocity_ij[k] = mac / (4 * np.pi) * \
-        ( r2_cross_product[k] / (np.absolute(ri2j[k])*(np.absolute(ri2j[k])-r2_dot_product)) \
-        - r1_cross_product[k] / (np.absolute(ri1j[k])*(np.absolute(ri1j[k])-r1_dot_product)) )
+    ri1j_abs = npla.norm(ri1j)
+    ri2j_abs = npla.norm(ri2j)
+    r1_cross_prod = np.cross(v_inf, ri1j)
+    r2_cross_prod = np.cross(v_inf, ri2j)
+    r1_dot_prod = np.dot(v_inf, ri1j)
+    r2_dot_prod = np.dot(v_inf, ri2j)
+    
+    velocity_ij = mac / (4 * np.pi) * \
+        ( r2_cross_prod / (ri2j_abs*(ri2j_abs-r2_dot_prod)) \
+        - r1_cross_prod / (ri1j_abs*(ri1j_abs-r1_dot_prod)) )
+    
+    if not same_panel_check:
+        r12_cross_prod = np.cross(ri1j, ri2j)
+        r12_dot_prod = np.dot(ri1j, ri2j)
+        velocity_ij +=  mac / (4 * np.pi) * \
+            ( (ri1j_abs + ri2j_abs) * r12_cross_prod / (ri1j_abs * ri2j_abs * (ri1j_abs*ri2j_abs + r12_dot_prod)) )
+    
+    return velocity_ij
