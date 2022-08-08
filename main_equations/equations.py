c3688745bc72336874115d392d86b90ba37e3b86
kuodrek <kuodrek@gmail.com>
2022-08-07 21:08:42 -0300

    added mockup for main_equation

--- main_equations/equations.py
+++ main_equations/equations.py
@@ -4,7 +4,17 @@ from models.simulation import Simulation
 from models.flight_conditions import FlightConditions
 
 def main_equation(Wing: Wing, Simulation: Simulation, FlightConditions: FlightConditions):
-    residual = 0
+    N = Wing.N_panels
+    for i in range(N):
+        # Calcular velocidades induzidas
+        vij = 1
+        # Calcular angulos de ataque de cada seçao
+        aoa_i = 1
+        # Calcular os Cl's de cada seção
+        cl_i = 1
+        # Calculo do valor residual da funçao
+        residual = 0
+
     return residual
 
 def corrector_equation(Wing: Wing, Simulation: Simulation, FlightConditions: FlightConditions):
