5407e83be0d83ac22cdb3348546a0f5601eb7198
Luis Kuodrek <kuodrek@gmail.com>
2022-08-08 19:37:44 -0300

    added v_ij_distr argument

--- main_equations/equations.py
+++ main_equations/equations.py
@@ -1,32 +1,34 @@
 import numpy as np
+import numpy.linalg as npla
 from models.wing import Wing
 from models.simulation import Simulation
 from models.flight_conditions import FlightConditions
 from utils import velocity
 
-def main_equation(Wing: Wing, Simulation: Simulation, FlightConditions: FlightConditions):
+def main_equation(Wing: Wing, FlightConditions: FlightConditions, v_ij_distr, G):
     N_panels = Wing.N_panels
     collocation_points = Wing.collocation_points
     vertice_points = Wing.vertice_points
-    v_inf = 1
-    mac = 1
+    u_n = Wing.u_n
+    u_a = Wing.u_a
+    cp_dsl = Wing.cp_dsl
+    cp_macs = Wing.cp_macs # Verificar se usa o mac de cada painel ou o MAC global
+    v_inf = np.array([1, 0, 0])
+
+    residual = np.zeros(N_panels)
+    velocity_sum = np.zeros(3)
     for i in range(N_panels):
-        # Calcular velocidades induzidas
-        cp_i = collocation_points[i]
-        for j in range(N_panels):
-            vp_i = vertice_points[j]
-            vp_ii = vertice_points[j+1]
-            v_ij = velocity.get_induced_velocity(cp_i, vp_i, vp_ii, v_inf, mac)
-        # Calcular angulos de ataque de cada seçao
-        aoa_i = 1
-        # Calcular os Cl's de cada seção
+        v_ij_panel = v_ij_distr[i,:,:]
+        velocity_sum = v_ij_panel * G[j]
+        aoa_i = np.arctan(np.dot(velocity_sum+v_inf, u_n[i]) / np.dot(velocity_sum+v_inf, u_a[i]))
+        # Calcular o Cl da seção
         cl_i = 1
-        # Calculo do valor residual da funçao
-        residual = 0
+        # Calculo do valor residual da função
+        residual[i] = 2 * npla.norm( np.cross(velocity_sum+v_inf, cp_dsl[i]) )*G[i] - cl_i
 
     return residual
 
-def corrector_equation(Wing: Wing, Simulation: Simulation, FlightConditions: FlightConditions):
+def corrector_equation(Wing: Wing, FlightConditions: FlightConditions):
     delta_G = 0
     return delta_G
     
