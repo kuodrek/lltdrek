a5a42db0fe41c34bc0102b75180ea6db4a4bdf0c
Luis Kuodrek <luis.kuodrek@indicium.tech>
2022-08-08 11:32:06 -0300

    added induced velocity function setup

--- main_equations/equations.py
+++ main_equations/equations.py
@@ -2,12 +2,21 @@ import numpy as np
 from models.wing import Wing
 from models.simulation import Simulation
 from models.flight_conditions import FlightConditions
+from utils import velocity
 
 def main_equation(Wing: Wing, Simulation: Simulation, FlightConditions: FlightConditions):
-    N = Wing.N_panels
-    for i in range(N):
+    N_panels = Wing.N_panels
+    collocation_points = Wing.collocation_points
+    vertice_points = Wing.vertice_points
+    v_inf = 1
+    mac = 1
+    for i in range(N_panels):
         # Calcular velocidades induzidas
-        vij = 1
+        cp_i = collocation_points[i]
+        for j in range(N_panels):
+            vp_i = vertice_points[j]
+            vp_ii = vertice_points[j+1]
+            v_ij = velocity.get_induced_velocity(cp_i, vp_i, vp_ii, v_inf, mac)
         # Calcular angulos de ataque de cada seçao
         aoa_i = 1
         # Calcular os Cl's de cada seção
