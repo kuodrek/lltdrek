0092c9a955446f863a03c02dfc953f4aa6b2e0d9
kuodrek <kuodrek@gmail.com>
2022-08-08 22:18:06 -0300

    added v_inf_array

--- models/flight_conditions.py
+++ models/flight_conditions.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 class FlightConditions:
     """
     Velocidade do escoamento - Vinf
@@ -6,8 +9,13 @@ class FlightConditions:
     altura do chao - h
     angulo de ataque - aoa
     """
-    def __init__(self, V_inf, nu, rho, h=0):
+    def __init__(self, V_inf, nu, rho, aoa, h=0):
         FlightConditions.V_inf = V_inf
         FlightConditions.nu = nu
         FlightConditions.rho = rho
+        FlightConditions.aoa = aoa
         FlightConditions.h = h
+        FlightConditions.v_inf_array = np.zeros([len(aoa), 3])
+        for i, aoa_i in enumerate(aoa):
+            aoa_i = aoa_i * np.pi / 180
+            self.v_inf_array[i,:] = [np.cos(aoa_i), 0, np.sin(aoa_i)]
