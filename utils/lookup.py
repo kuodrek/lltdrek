360472d63ee4a1fe099d716b25dc7fbab3bd4f7c
kuodrek <kuodrek@gmail.com>
2022-08-27 16:29:38 -0300

    finished lookup functions v1

--- utils/lookup.py
+++ utils/lookup.py
@@ -1,56 +1,31 @@
-airfoils_data_dict = {
-    "naca0012": {
-        "1.2e5": {
-            "cl_list": [[1, 1.1], [2, 1.3], [3, 1.35], [4, 1.4], [5, 1.46]],
-            "cl_alfa": 1,
-            "cl0": 1,
-            "cm0": 1,
-        },
-        "1e6": {
-            "cl_list": [[1, 1.14], [2, 1.34], [3, 1.33], [4, 1.5], [5, 1.6]],
-            "cl_alfa": 1,
-            "cl0": 1,
-            "cm0": 1,
-        },
-        "1e7": {
-            "cl_list": [[1, 1.2], [2, 1.5], [3, 1.7], [4, 1.8], [5, 1.9]],
-            "cl_alfa": 1,
-            "cl0": 1,
-            "cm0": 1,
-        },
-    },
-    "s1223": {
-        "1.2e5": {
-            "cl_list": [[1, 1.1], [2, 1.3], [3, 1.35], [4, 1.4], [5, 1.46]],
-            "cl_alfa": 1,
-            "cl0": 1,
-            "cm0": 1,
-        },
-        "1e6": {
-            "cl_list": [[1, 1.14], [2, 1.34], [3, 1.33], [4, 1.5], [5, 1.6]],
-            "cl_alfa": 1,
-            "cl0": 1,
-            "cm0": 1,
-        },
-        "1e7": {
-            "cl_list": [[1, 1.2], [2, 1.5], [3, 1.7], [4, 1.8], [5, 1.9]],
-            "cl_alfa": 1,
-            "cl0": 1,
-            "cm0": 1,
-        },
-    },
-}
-
-
 def get_cl(airfoil_data_dict, reynolds_number, aoa):
     reynolds_list = list(airfoil_data_dict)
-    for reynolds_i, cl_data in airfoil_data_dict.items():
-        reynolds_i = int(reynolds_i)
+    reynolds_list = [float(reynolds) for reynolds in reynolds_list]
+    if reynolds_number <= reynolds_list[0]:
+        cl_data = airfoil_data_dict[reynolds_list[0]]
+        cl = aoa_list_lookup(cl_data, aoa)
+    elif reynolds_number >= reynolds_list[-1]:
+        cl_data = airfoil_data_dict[reynolds_list[-1]]
+        cl = aoa_list_lookup(cl_data, aoa)
+    else:
+        for i in range(len(reynolds_list)-1):
+            re_i = reynolds_list[i]
+            re_ii = reynolds_list[i+1]
+            if re_i <= reynolds_number <= re_ii:
+                cl_data_i = airfoil_data_dict[reynolds_list[i]]
+                cl_data_ii = airfoil_data_dict[reynolds_list[i+1]]
+
+                cl_i = aoa_list_lookup(cl_data_i, aoa)
+                cl_ii = aoa_list_lookup(cl_data_ii, aoa)
+                
+                cl = (cl_ii - cl_i)/(re_ii - re_i) * (reynolds_number - re_i) + cl_i
+                break
+    return cl
 
 
-def aoa_list_lookup(cl_list, aoa):
+def aoa_list_lookup(cl_data, aoa):
+    cl_list = cl_data["cl_list"]
     if aoa <= cl_list[0][0]:
-        # cl = ( cl_lista(2,2)-cl_lista(1,2) )/( cl_lista(2,1)-cl_lista(1,1) )*(aoa-cl_lista(1,1)) + cl_lista(1,2);
         aoa_i = cl_list[0][0]
         aoa_ii = cl_list[1][0]
         cl_i = cl_list[0][1]
@@ -69,12 +44,5 @@ def aoa_list_lookup(cl_list, aoa):
                 cl_ii = cl_list[i+1][1]
                 break
     
-    cl_interp = (cl_ii - cl_i) / (aoa_ii - aoa_i) * (aoa - aoa_i) + aoa_i
+    cl_interp = (cl_ii - cl_i) / (aoa_ii - aoa_i) * (aoa - aoa_i) + cl_i
     return cl_interp
-
-# cl = get_cl(airfoils_data_dict)
-for index, items in airfoils_data_dict["s1223"].items():
-    print(f"item: {items}")
-    print(f"index: {index}")
-
-print(list(airfoils_data_dict["s1223"]))
