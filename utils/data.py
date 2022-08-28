32e347885439f223835930fc05b4b2209be66d75
kuodrek <kuodrek@gmail.com>
2022-08-28 18:57:43 -0300

    updated data load functions

---
+++ utils/data.py
@@ -0,0 +1,14 @@
+import os
+import sys
+
+def load_data():
+    current_dir = os.path.abspath(os.getcwd())
+    print(current_dir)
+
+
+def load_cl_file():
+    pass
+
+
+def load_dat_file():
+    pass
