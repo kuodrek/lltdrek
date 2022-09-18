19d61dc07ce6895dc464a45ffdfff4d2d4fc2b11
Luis Kuodrek <luis.kuodrek@incidicium.tech>
2022-09-18 00:20:30 -0300

    update calculate_induced_velocities

--- models/wingpool.py
+++ models/wingpool.py
@@ -5,15 +5,8 @@ from utils import velocity
 
 
 wingpool_dict = {
-    "flight_condition": {
-        "ground_effect_check": False,
-        "height": "height_value"
-    },
-    "surfaces": {
     "surface_name": {
-        "id": 1,
-        "wing": "wing_object",
-    }
+        "surface_name": "v_ind"
     }
 }
 
@@ -21,17 +14,14 @@ class WingPool:
     def __init__(self, wing_list: List[Wing], flight_condition: FlightCondition):
         WingPool.wing_list = wing_list
         WingPool.flight_condition = flight_condition
-        WingPool.wingpool_dict = None
+        WingPool.wingpool_velocities = None
     
-    def generate_wingpool_dict(self):
-        # Function that returns wingpool_dict
-        wingpool_dict = {}
-        wingpool_dict["flight_condition"]["ground_effect_check"] = self.flight_condition.ground_effect_check
-        wingpool_dict["flight_condition"]["ground_effect_check"] = self.flight_condition.h
-        for wing in self.wing_list:
-            wingpool_dict["surfaces"][wing.surface_name] = 
-            pass
-        pass
 
     def calculate_induced_velocities(self):
-        pass
+        for wing_cp in self.wing_list:
+            wingpool_velocities = {}
+            for wing_vp in self.wing_list:
+                v_ind_vetor = 1
+                wingpool_velocities[wing_cp.surface_name][wing_vp.surface_name] = v_ind_vetor
+        self.wingpool_velocities = wingpool_velocities
+        
