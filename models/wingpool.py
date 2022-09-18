49b91dff60c83bf37368ac92029daa617bbacb2e
Luis Kuodrek <luis.kuodrek@incidicium.tech>
2022-09-18 11:39:48 -0300

    update wingpool class

--- models/wingpool.py
+++ models/wingpool.py
@@ -4,24 +4,39 @@ from models.flight_condition import FlightCondition
 from utils import velocity
 
 
-wingpool_dict = {
+wingpool_dict_template = {
     "surface_name": {
-        "surface_name": "v_ind"
+        "surface_name": "v_ind_distr"
     }
+}
+
+G_dict = {
+    "surface_name": "G_array",
 }
 
 class WingPool:
     def __init__(self, wing_list: List[Wing], flight_condition: FlightCondition):
         WingPool.wing_list = wing_list
         WingPool.flight_condition = flight_condition
-        WingPool.wingpool_velocities = None
-    
+        WingPool.velocities_dict = None
+        WingPool.G_dict = {}
+        # TODO: implement a history_check variable to keep history of solutions (G_dict)
+
 
     def calculate_induced_velocities(self):
+        v_inf = self.flight_condition.v_inf_array
         for wing_cp in self.wing_list:
-            wingpool_velocities = {}
+            velocities_dict = {}
+            collocation_points = wing_cp.collocation_points
+            cp_macs = wing_cp.cp_macs
             for wing_vp in self.wing_list:
-                v_ind_vetor = 1
-                wingpool_velocities[wing_cp.surface_name][wing_vp.surface_name] = v_ind_vetor
-        self.wingpool_velocities = wingpool_velocities
-        
+                vertice_points = wing_vp.vertice_points
+                v_ij_distr = velocity.get_induced_velocity_distribution(
+                    collocation_points, cp_macs, vertice_points, v_inf
+                )
+                velocities_dict[wing_cp.surface_name][wing_vp.surface_name] = v_ij_distr
+        self.velocities_dict = velocities_dict
+    
+
+    def update_solution(self, G, surface_name: str):
+        self.G_dict[surface_name] = G
