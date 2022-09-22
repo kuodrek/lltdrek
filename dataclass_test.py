ff86f1628e871f0b68808489a16ae5df7c4c2b53
Luis Kuodrek <kuodrek@gmail.com>
2022-09-21 23:35:48 -0300

    update dataclass test

--- dataclass_test.py
+++ dataclass_test.py
@@ -1,30 +1,31 @@
-from dataclasses import dataclass
-
-@dataclass(frozen=True)
-class ImmutableClass:
-    int_number: int
-    str_value: str
+from models.wingpool_dataclass import WingPool
+from models.wing import Wing
+from models.flight_condition import FlightCondition
 
 
-@dataclass(frozen=False)
-class MutableClass:
-    int_number: int
-    str_value: str
+asa = Wing(
+        spans=[3, 2],
+        chords=[1, 0.8, 0.4],
+        offsets=[0, 0, 0.5],
+        twist_angles=[0, 0, 0],
+        dihedral_angles=[10, 15],
+        airfoils=['optfoilb2', 'optfoilb2', 'optfoilb2'],
+        N_panels=12,
+        distribution_type="cosine",
+        sweep_check=False,
+        surface_name='asa'
+    )
 
+asa.generate_mesh()
 
-# Testing immutable class
-aaa = ImmutableClass(int_number=1, str_value='yo')
-bbb = ImmutableClass(int_number=2, str_value='ye')
+flight_condition = FlightCondition(
+    V_inf=15,
+    nu=1.5e-5,
+    rho=1.225,
+    aoa = [1],
+    ground_effect_check=False
+)
 
-print(aaa.int_number)
-print(bbb.int_number)
-print(aaa.str_value)
-print(bbb.str_value)
-
-ccc = MutableClass(int_number=1, str_value='yo')
-ddd = MutableClass(int_number=2, str_value='ye')
-
-print(ccc.int_number)
-print(ddd.int_number)
-print(ccc.str_value)
-print(ddd.str_value)
+wingpool_teste = WingPool([asa], flight_condition)
+wingpool_teste.calculate_induced_velocities()
+a = 1
