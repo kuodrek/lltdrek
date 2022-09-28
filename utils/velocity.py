a0e9782485a64b9fa1bc6c005a46693b006194e8
Luis Kuodrek <luis.kuodrek@incidicium.tech>
2022-09-27 23:51:16 -0300

    update vij distr function

--- utils/velocity.py
+++ utils/velocity.py
@@ -5,13 +5,13 @@ from models.wing import Wing
 
 # Distribution of induced velocities
 def get_induced_velocity_distribution(collocation_points, cp_macs, vertice_points, v_inf):
-    v_ij_distr = np.zeros([len(collocation_points),len(vertice_points),3])
+    v_ij_distr = np.zeros([len(collocation_points),len(vertice_points)-1,3])
     len_cp = len(collocation_points)
-    len_vp = len(vertice_points) - 1
+    len_vp = len(vertice_points)
     for i in range(len_cp):
         cp_i = collocation_points[i]
         mac_i = cp_macs[i]
-        for j in range(len_vp):
+        for j in range(len_vp - 1):
             vp_j = vertice_points[j]
             vp_jj = vertice_points[j+1]
             v_ij = get_induced_velocity(cp_i, vp_j, vp_jj, mac_i, v_inf)
@@ -46,12 +46,12 @@ def get_induced_velocity(collocation_point, vertice_point_1, vertice_point_2, ma
     r12_cross_prod = np.cross(ri1j, ri2j)
     r12_dot_prod = np.dot(ri1j, ri2j)
 
-    bound_vortex_den = (ri1j_abs*ri2j_abs*(ri1j_abs*ri2j_abs+r12_dot_prod))
-
     velocity_ij = mac / (4 * np.pi) * \
         ( r2_cross_prod / (ri2j_abs*(ri2j_abs-r2_dot_prod)) \
         - r1_cross_prod / (ri1j_abs*(ri1j_abs-r1_dot_prod)) )
     
+    bound_vortex_den = (ri1j_abs*ri2j_abs*(ri1j_abs*ri2j_abs+r12_dot_prod))
+
     if bound_vortex_den != 0:
         velocity_ij +=  mac / (4 * np.pi) * \
             ( (ri1j_abs+ri2j_abs) * r12_cross_prod / bound_vortex_den )
