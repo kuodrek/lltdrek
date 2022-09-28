a7ee304e63f6665e8014838c7a4128d4a916052f
Luis Kuodrek <luis.kuodrek@incidicium.tech>
2022-09-27 23:50:21 -0300

    update test

--- teste_vij_distr.py
+++ teste_vij_distr.py
@@ -2,14 +2,13 @@ import numpy as np
 from models.wing import Wing
 from models.flight_condition import FlightCondition
 from utils import velocity
-import copy
 
 asa = Wing(
         spans=[3, 2],
-        chords=[1, 0.8, 0.4],
-        offsets=[0, 0, 0.5],
+        chords=[1, 1, 1],
+        offsets=[0, 0, 0],
         twist_angles=[0, 0, 0],
-        dihedral_angles=[10, 15],
+        dihedral_angles=[0, 0],
         airfoils=['optfoilb2', 'optfoilb2', 'optfoilb2'],
         N_panels=12,
         distribution_type="cosine",
@@ -18,15 +17,13 @@ asa = Wing(
     )
 
 asa.generate_mesh()
-asa = copy.deepcopy(asa)
-print(len(asa.collocation_points))
 
 asa_2 = Wing(
         spans=[3, 2],
-        chords=[1, 0.8, 0.4],
-        offsets=[0, 0, 0.5],
+        chords=[1, 1, 1],
+        offsets=[0, 0, 0],
         twist_angles=[0, 0, 0],
-        dihedral_angles=[10, 15],
+        dihedral_angles=[0, 0],
         airfoils=['optfoilb2', 'optfoilb2', 'optfoilb2'],
         N_panels=20,
         distribution_type="cosine",
@@ -37,7 +34,6 @@ asa_2 = Wing(
     )
 
 asa_2.generate_mesh()
-print(len(asa_2.collocation_points))
 
 flight_condition = FlightCondition(
     V_inf=15,
@@ -47,16 +43,11 @@ flight_condition = FlightCondition(
     ground_effect_check=False
 )
 
-cp = asa.collocation_points[0]
-vp_1 = asa.vertice_points[0]
-vp_2 = asa.vertice_points[1]
-cp_mac = asa.cp_macs[0]
 v_inf = flight_condition.v_inf_array
 
-# Testing single induced velocity
-velocity.get_induced_velocity(cp, vp_1, vp_2, cp_mac, v_inf)
-
 # Testing distribution of induced velocities
+v_ij_distr_self = velocity.get_induced_velocity_distribution(asa.collocation_points, asa.cp_macs, asa.vertice_points, v_inf)
+
 v_ij_distr = velocity.get_induced_velocity_distribution(asa.collocation_points, asa.cp_macs, asa_2.vertice_points, v_inf)
 
 a = 1
