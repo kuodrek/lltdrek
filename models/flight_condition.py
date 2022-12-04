c936cb3f03186f62c81e5fa669f1d4a5255567b0
luis-kuodrek-bk <ext.luis.kuodrek@burgerking.com.br>
2022-12-03 21:05:48 -0300

    update attribute v_inf_array to v_inf_list

--- models/flight_condition.py
+++ models/flight_condition.py
@@ -11,10 +11,10 @@ class FlightCondition:
     aoa: List[float]
     h: float = 0
     ground_effect_check: bool = False
-    v_inf_array: np.ndarray = field(init=False)
+    v_inf_list: np.ndarray = field(init=False)
 
     def __post_init__(self):
-        self.v_inf_array = np.zeros([len(self.aoa), 3])
+        self.v_inf_list = np.zeros((len(self.aoa), 3))
         for i, aoa_i in enumerate(self.aoa):
             aoa_i = aoa_i * np.pi / 180
-            self.v_inf_array[i,:] = [np.cos(aoa_i), 0, np.sin(aoa_i)]
+            self.v_inf_list[i,:] = [np.cos(aoa_i), 0, np.sin(aoa_i)]
