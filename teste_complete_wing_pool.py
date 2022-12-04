770da0f7f4056624c6f3c7300170d145a1464143
Luis Kuodrek <kuodrek@gmail.com>
2022-12-04 19:29:52 -0300

    update test file to run a simulation

--- teste_complete_wing_pool.py
+++ teste_complete_wing_pool.py
@@ -2,7 +2,11 @@ import numpy as np
 from models.wing import Wing
 from models.flight_condition import FlightCondition
 from models.wingpool import WingPool
-from utils import velocity
+from models.simulation import Simulation
+from utils import data
+
+
+airfoils_data_dict, airfoils_dat_dict = data.load_folder('airfoils_test')
 
 # Testing terms of the newton corrector equation and its symmetries
 
@@ -12,7 +16,7 @@ asa = Wing(
         offsets=[0, 0, 0],
         twist_angles=[0, 0, 0],
         dihedral_angles=[0, 0],
-        airfoils=['optfoilb2', 'optfoilb2', 'optfoilb2'],
+        airfoils=['naca64210', 'naca64210', 'naca2412'],
         N_panels=12,
         distribution_type="cosine",
         sweep_check=False,
@@ -20,6 +24,7 @@ asa = Wing(
     )
 
 asa.generate_mesh()
+
 flight_condition = FlightCondition(
     V_inf=15,
     nu=1.5e-5,
@@ -28,16 +33,23 @@ flight_condition = FlightCondition(
     ground_effect_check=False
 )
 
+asa.setup_airfoil_data(flight_condition, airfoils_data_dict)
+
 wingpool = WingPool(
     wing_list=[asa],
     flight_condition=flight_condition
 )
 
+simulation = Simulation(
+    wing_pool=wingpool,
+)
 
-v_inf_array = flight_condition.v_inf_list[0]
+simulation.run_simulation()
 
-# testando métodos que atualizam dicionarios de velocidade total e distribuiçao de alfa
-total_velocity_dict = wingpool.calculate_total_velocity(v_inf_array)
-wingpool.calculate_aoa_eff(total_velocity_dict)
+# v_inf_array = flight_condition.v_inf_list[0]
+
+# # testando métodos que atualizam dicionarios de velocidade total e distribuiçao de alfa
+# total_velocity_dict = wingpool.calculate_total_velocity(v_inf_array)
+# wingpool.calculate_aoa_eff(total_velocity_dict)
 
 a=1
