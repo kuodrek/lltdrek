e0607f41fa3a437d775b188fe8558b41eaab570e
Luis Kuodrek <kuodrek@gmail.com>
2022-12-06 22:34:40 -0300

    create post processing methods

--- models/post_processing.py
+++ models/post_processing.py
@@ -1,8 +1,26 @@
 from typing import List
 import numpy as np
 from dataclasses import dataclass, field
-
+from models.wingpool import WingPool
+from models.wing import Wing
+from models.flight_condition import FlightCondition
 
 @dataclass
 class PostProcessing:
-    pass
+    reference_point: list
+
+
+    def __post_init__(self) -> None:
+        pass
+
+
+    def get_coefficients(self, wing_pool: WingPool, G_solution: list) -> dict:
+        pass
+
+
+    def get_CL_max_linear(self, wing_pool: WingPool, G_solution: list) -> float:
+        pass
+
+
+    def get_aerodynamic_center(self, wing_pool: WingPool, G_solution: list) -> float:
+        pass
