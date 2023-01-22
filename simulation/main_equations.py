b810614fec834b5a7b5db968dba41e5bda763f2d
kuodrek <kuodrek@gmail.com>
2023-01-21 21:15:49 -0300

    fix and update equations functions

--- simulation/main_equations.py
+++ simulation/main_equations.py
@@ -50,21 +50,7 @@ def calculate_main_equation(
     """
     R_array = np.zeros(matrix_dim)
     i_glob = 0
-    Cl_list_matlab = [
-        -0.260511170622977,
-        -0.268345918330110,
-        -0.281624237725908,
-        -0.299603660736258,
-        -0.321053381257372,
-        -0.345232324624992,
-        -0.372356495406586,
-        -0.403673250670296,
-        -0.442496513378669,
-        -0.482057602179404,
-        -0.566033419754520,
-        -1.76094339959853,
-    ]
-    Cl_list = []
+    Cl_list = [] # Used for debugging purposes
     for wing in wing_pool.complete_wing_pool:
         if "_mirrored" in wing.surface_name: continue
         N_panels = wing.N_panels
@@ -84,11 +70,6 @@ def calculate_main_equation(
                 - Cl_i
             R_array[N_panels+i_glob] = R_array[i_glob]
             i_glob += 1
-    R_array_1 = R_array[0:12]
-    R_array_2 = R_array[12:25]
-    diff = R_array_1 - R_array_2
-    if abs(diff.max()) > 1e-1:
-        a=1
     return R_array
 
 
@@ -100,7 +81,7 @@ def calculate_corrector_equation(
     G_dict: dict,
     aoa_idx: int,
     wing_pool: WingPool,
-    matrix_dim: int
+    matrix_dim: int,
 ) -> np.ndarray:
     """
     Newton corrector system of equations
@@ -108,20 +89,7 @@ def calculate_corrector_equation(
     """
     J_matrix = np.zeros([matrix_dim, matrix_dim])
     i_glob = 0
-    cl_alpha_list_matlab = np.array([
-        6.56432695000082,
-        5.77997288887701,
-        5.64677358626603,
-        5.62687650175797,
-        5.62216700522713,
-        5.68953289211780,
-        5.84405073097534,
-        6.07940733620502,
-        6.36720007295177,
-        4.18345756474756,
-        4.02263955121260,
-        5.21895956133639,
-    ])
+    Cl_alpha_list = [] # Used for debugging purposes
     for wing_i in wing_pool.complete_wing_pool:
         G_distr = G_dict[wing_i.surface_name]
         total_velocity_distr = total_velocity_dict[wing_i.surface_name]
@@ -142,6 +110,7 @@ def calculate_corrector_equation(
                     wing_i.airfoil_data,
                     cl_alpha_check = True
                 ) * 180 / np.pi
+            Cl_alpha_list.append(Cl_alpha_i)
             for wing_j in wing_pool.complete_wing_pool:
                 if "_mirrored" in wing_j.surface_name: continue
                 v_ij_distr = wing_pool.ind_velocities_list[aoa_idx][wing_i.surface_name][wing_j.surface_name]
@@ -158,12 +127,9 @@ def calculate_corrector_equation(
     
                     if wing_i.parent_wing == wing_j.surface_name and i_glob == (N_panels+j_glob):
                         coef_ij_m += 2 * w_i_abs
-                        # if "_mirrored" in wing_i.surface_name: # Essa parcela está sendo desconsiderada no llt original. Por quê?
-                        #     coef_ij = 0
 
                     if wing_i.surface_name == wing_j.surface_name and i == j:
                         coef_ij += 2 * w_i_abs
-                        coef_ij_m = 0
                     
                     J_matrix[i_glob][j_glob] = coef_ij
                     J_matrix[i_glob][N_panels+j_glob] = coef_ij_m
@@ -171,9 +137,4 @@ def calculate_corrector_equation(
             i_glob += 1
 
     delta_G = npla.solve(J_matrix, -R_array)
-    delta_G_1 = delta_G[0:12]
-    delta_G_2 = delta_G[12:25]
-    diff = delta_G_1 - delta_G_2
-    if abs(diff.max()) > 1e-1:
-        a=1
     return delta_G
