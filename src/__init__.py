c7ae1d13643a8938b11b814f048fbdfeff15fd5b
Kuodrek <kuodrek@gmail.com>
2025-02-16 22:44:30 -0300

    rename to src

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+from .models.flight_condition import FlightCondition
+from .models.simulation import Simulation
+from .models.post_processing import PostProcessing
+from .models.wing import Wing
+from .models.wingpool import WingPool
