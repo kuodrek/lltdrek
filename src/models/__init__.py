fed789b7bd0503050e58ce3a2615c590fc56f093
Kuodrek <kuodrek@gmail.com>
2025-03-01 13:50:31 -0300

    comment out improts

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,7 +1,5 @@
-from .flight_condition import FlightCondition
-from .simulation import Simulation
-from .post_processing import PostProcessing
-from .wing import Wing
-from .wingpool import WingPool
-
-from .types import AOA
+# from .flight_condition import FlightCondition
+# from .simulation import Simulation
+# from .post_processing import PostProcessing
+# from .wing import Wing
+# from .wingpool import WingPool
