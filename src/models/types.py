c7e9c1b5d9248f3a74a23b7419e6c8b07fd99c12
Kuodrek <kuodrek@gmail.com>
2025-03-03 16:45:31 -0300

    add DVS types

--- src/models/types.py
+++ src/models/types.py
@@ -1,3 +1,8 @@
-from typing import Union
+from typing import Union, Dict
+import numpy as np
 
-AOA = Union[int, float]
+AngleOfAttack = Union[int, float]
+
+# DVS Stands for Dimensionless Vortex Strenghts. These are the values we wish to find by solving the main equation
+DVSArray = np.ndarray # Unmapped array of DVS from Main Equation
+DVSMap = Dict[str, np.ndarray] # Map of DVS values, assigned to each wing in a wing pool
