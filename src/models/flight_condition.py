9094031909f7374cdcf5648554f1a2eb96b71df4
Kuodrek <kuodrek@gmail.com>
2025-03-03 16:45:21 -0300

    add documentation

--- src/models/flight_condition.py
+++ src/models/flight_condition.py
@@ -1,14 +1,31 @@
 from typing import Union, Sequence
+from src.models.types import AngleOfAttack
 import numpy as np
 
 class FlightCondition:
+    """Represents atmospheric conditions (air speed, density, etc) for a given
+    wing pool
 
+    :param V_inf: Free stream velocity
+    :type V_inf: Union[float, int]
+    :param nu: Air kinematic viscosity
+    :type nu: Union[float, int]
+    :param rho: Air density
+    :type rho: Union[float, int]
+    :param angles_of_attack: Sequence of angles of attack that will be used for simulation
+    :type angles_of_attack: Sequence[AngleOfAttack]
+    :param h: Height in reference to coordinates ```(x=0, z=0)``` in a wing pool system of surfaces.
+    Used for ground effect calculations
+    :type h: Union[float, int]
+    :param ground_effect_check: If True, panel velocity calculation will take ground proximity into account
+    :type ground_effect_check: bool = False
+    """
     def __init__(
         self,
         V_inf: Union[float, int],
         nu: Union[float, int],
         rho: Union[float, int],
-        angles_of_attack: Sequence[Union[float, int]],
+        angles_of_attack: Sequence[AngleOfAttack],
         h: Union[float, int],
         ground_effect_check: bool = False,
     ):
