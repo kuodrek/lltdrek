7347595904ceb124556b41056ffc4268722847ab
Kuodrek <kuodrek@gmail.com>
2025-03-04 01:35:15 -0300

    add alpha not found exception

--- src/models/exceptions.py
+++ src/models/exceptions.py
@@ -1,3 +1,10 @@
-# Wing Pool exceptions
+# Flight condition exceptions
+class AlphaNotFoundException(IndexError):
+    def __init__(self, alpha, angles_of_attack):
+        super().__init__(
+            f"Angle of attack {alpha} not found in angles_of_attack array: {angles_of_attack}"
+        )
 
+
+# Wing Pool exceptions
 class NonUniqueWingsException(Exception): ...
