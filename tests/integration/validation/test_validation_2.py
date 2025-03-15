74eca973ad66f98a750afbbe2e2e7e3a0966d5bc
drek <kuodrek@gmail.com>
2025-03-15 18:27:21 -0300

    add validation tests

---
+++ tests/integration/validation/test_validation_2.py
@@ -0,0 +1,11 @@
+"""NEELY, Robert H.; BOLLECH, Thomas V.; WESTRICK, Gertrude C. Experimental and
+calculated characteristics of several NACA 44-series wings with aspect ratios of 8, 10,
+and 12 and taper ratios of 2.5 and 3.5. NATIONAL AERONAUTICS AND SPACE
+ADMIN LANGLEY RESEARCH CENTER HAMPTON VA, 1947. Disponível em:
+< https://ntrs.nasa.gov/search.jsp?R=19930081871>. Acesso em 22 out 2019."""
+
+"""SIVELLS, James C. Experimental and Calculated Characteristics of Three Wings of
+NACA 64-210 and 65-210 Airfoil Sections with and without 2 deg Washout. NATIONAL
+AERONAUTICS AND SPACE ADMIN LANGLEY RESEARCH CENTER HAMPTON
+VA, 1947. Disponível em: < https://apps.dtic.mil/dtic/tr/fulltext/u2/a801248.pdf>. Acesso em
+22 out 2019."""
